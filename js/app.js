3f2eceb41ded33fb08e242c1521f03f6c2b9ab44
daniel novak <datnovak@gmail.com>
2018-07-08 10:29:23 +0200

    Update Enemy and Player objects so they can be drawn on canvas

--- js/app.js
+++ js/app.js
@@ -6,14 +6,19 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    // Enemy init position is 1st-3rd row, out of canvas
+    this.x = -202;
+    this.y = (83 * (Math.floor(Math.random() * Math.floor(3)))) + 55;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+    // Multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += 60 * dt;
+    // TODO collision detection
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +29,47 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+  // Player init position is in 5th row, 3rd column
+  this.x = (2 * 101);
+  this.y = (4 * 83) + 45;
+};
 
+Player.prototype.update = function() {
+  // noop
+};
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.handleInput = function(keyPressed) {
+  // TODO limit movement inside of game area
+  // TODO reset game when player has reached water
+  switch (keyPressed) {
+    case 'left':
+      this.x -= 101;
+      break
+    case 'up':
+      this.y -= 83;
+      break
+    case 'right':
+      this.x += 101;
+      break
+    case 'down':
+      this.y += 83;
+  }
+};
 
+// Draw the Player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Instantiate enemy and player objects.
+var player = new Player();
+var allEnemies = [];
+
+// TODO continous enemy creation
+var nmy = new Enemy();
+allEnemies.push(nmy);
 
 
 // This listens for key presses and sends the keys to your
